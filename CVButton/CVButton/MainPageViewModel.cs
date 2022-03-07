e86f89bb3740cd161e482d1e63732329a8ac11b2
Héctor Pérez <hectorperez_11c@cenidet.edu.mx>
2022-03-07 09:37:31 -0600

    Add project files.

---
+++ CVButton/CVButton/MainPageViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CVButton
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Person> People { get; set; }
+        public ICommand ClickedCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            People = new ObservableCollection<Person>
+            {
+                new Person
+                {
+                    Id = 1,
+                    Name = "Héctor"
+                },
+                new Person
+                {
+                    Id = 2,
+                    Name = "Peter"
+                },
+                new Person
+                {
+                    Id = 3,
+                    Name = "John"
+                }
+            };
+
+            ClickedCommand = new Command((data) =>
+            {
+                var i = data;
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
